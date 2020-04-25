0ee39cbe26a4b774c9d4e241b57f5b0ad7f24676
AvrisNox <noahd@mtu.edu>
2020-04-25 18:33:01 -0400

    Init
    True initialization of IntelliJ content.

---
+++ src/com/avrisnox/gamedev/mvc/build/Build.java
@@ -0,0 +1,11 @@
+package com.avrisnox.gamedev.mvc.build;
+
+import com.avrisnox.gamedev.mvc.controller.Controller;
+import com.avrisnox.gamedev.mvc.model.Model;
+import com.avrisnox.gamedev.mvc.view.View;
+
+public interface Build {
+	public Controller getController();
+	public Model getModel();
+	public View getView();
+}
