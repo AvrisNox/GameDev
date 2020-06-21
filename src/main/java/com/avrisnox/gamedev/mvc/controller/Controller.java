f6bfbe732f11cf86a1f809f80b4cec96dbd71931
AvrisNox <noahd@mtu.edu>
2020-06-20 22:34:40 -0400

    Comments!
    Threw a buncha comments up in here to make it easier to understand.

--- src/main/java/com/avrisnox/gamedev/mvc/controller/Controller.java
+++ src/main/java/com/avrisnox/gamedev/mvc/controller/Controller.java
@@ -2,6 +2,11 @@ package com.avrisnox.gamedev.mvc.controller;
 
 import com.avrisnox.gamedev.mvc.model.Model;
 
+/**
+ * Controller type for MV<bold>C</bold>.
+ * Should handle input from the user in whatever form the associated model will care about.
+ * @param <MType> The class type for the Model
+ */
 @SuppressWarnings("WeakerAccess")
 public abstract class Controller <MType extends Model> {
 	protected MType model;
@@ -9,28 +14,54 @@ public abstract class Controller <MType extends Model> {
 	protected GenericMouseHandler mouse;
 	protected GenericJoystickHandler joys;
 
+	/**
+	 * A class for handling a keyboard
+	 */
 	protected abstract class GenericKeyHandler {
 		protected abstract void update();
 	}
 
+	/**
+	 * A class for handling a mouse
+	 */
 	protected abstract class GenericMouseHandler {
 		protected abstract void update();
 	}
 
+	/**
+	 * A class for handling a joystick
+	 */
 	protected abstract class GenericJoystickHandler {
 		protected abstract void update();
 	}
 
+	/**
+	 * The default controller constructor.
+	 */
 	public Controller() { }
 
+	/**
+	 * Performs controller initialization logic.
+	 * @param model The model this controller will control
+	 */
 	public void init(MType model) {
 		this.model = model;
 	}
 
+	/**
+	 * Functionality to perform before updating.
+	 */
 	protected void preupdate() { }
 
+	/**
+	 * Functionality to perform after updating.
+	 */
 	protected void postupdate() { }
 
+	/**
+	 * The update method.
+	 * Do not call super.update() if you extend this - this function calls both pre and post update functions.
+	 */
 	public void update() {
 		preupdate();
 
--- src/main/java/com/avrisnox/gamedev/mvc/view/View.java
+++ src/main/java/com/avrisnox/gamedev/mvc/view/View.java
@@ -2,16 +2,33 @@ package com.avrisnox.gamedev.mvc.view;
 
 import com.avrisnox.gamedev.mvc.model.Model;
 
+/**
+ * Model type for M<bold>V</bold>C.
+ * @param <MType> The class type for the Model
+ */
 public abstract class View <MType extends Model> {
 	protected MType model;
 
+	/**
+	 * The default view constructor.
+	 */
 	public View() { }
 
+	/**
+	 * Performs view initialization logic.
+	 * @param model The model this view will control
+	 */
 	public void init(MType model) {
 		this.model = model;
 	}
 
+	/**
+	 * Performs view draw logic.
+	 */
 	public abstract void draw();
 
+	/**
+	 * Performs view destruction logic.
+	 */
 	public void destroy() { }
 }
